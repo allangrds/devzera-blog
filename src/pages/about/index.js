6d626123c6323a84529e652d2996d5d97c3e7d29
Allan Ramos <allangrds@gmail.com>
2020-06-03 15:06:45 -0300

    feat(about): add avatar image to page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,7 +1,10 @@
 import React, { memo } from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import Layout from '../../components/Layout'
 import Message from '../../components/Message'
+import SEO from '../../components/Seo'
 
 import FaceboookIcon from '../../assets/images/facebook.svg'
 import InstagramIcon from '../../assets/images/instagram.svg'
@@ -35,51 +38,74 @@ const SocialMedias = () => (
   </div>
 )
 
-const AboutPage = () => (
-  <Layout>
-    <Message
-      type="receive"
-      message={{
-        content: 'Quem é você?',
-        type: 'string',
-      }}
-    />
-    <Message
-      type="send"
-      message={{
-        content: 'Meu nome é Allan Ramos, nascido em Caraguatatuba/SP.Sou programador front-end.',
-        type: 'string',
-      }}
-    />
-    <Message
-      type="receive"
-      message={{
-        content: 'Qual o objetivo do blog?',
-        type: 'string',
-      }}
-    />
-    <Message
-      type="send"
-      message={{
-        content: 'Compartilhar os paranauê que eu aprender.',
-        type: 'string',
-      }}
-    />
-    <Message
-      type="receive"
-      message={{
-        content: 'Como posso te encontrar nas redes sociais?',
-        type: 'string',
-      }}
-    />
-    <Message
-      type="send"
-      message={{
-        content: <SocialMedias />,
-        type: 'node',
-      }}
-    />
-  </Layout>
-)
+const AboutPage = () => {
+  const { avatarImage } = useStaticQuery(
+    graphql`
+      query {
+        avatarImage: file(relativePath: { eq: "profile.jpeg" }) {
+          childImageSharp {
+            fluid(maxWidth: 300) {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        },
+      }
+    `
+  )
+
+  return (
+    <Layout>
+      <SEO title="Sobre | Devzera" />
+      <div className={style.image_container}>
+        <Img
+          className={style.avatar}
+          fluid={avatarImage.childImageSharp.fluid}
+        />
+      </div>
+      <Message
+        type="receive"
+        message={{
+          content: 'Quem é você?',
+          type: 'string',
+        }}
+      />
+      <Message
+        type="send"
+        message={{
+          content: 'Meu nome é Allan Ramos, nascido em Caraguatatuba/SP.Sou programador front-end.',
+          type: 'string',
+        }}
+      />
+      <Message
+        type="receive"
+        message={{
+          content: 'Qual o objetivo do blog?',
+          type: 'string',
+        }}
+      />
+      <Message
+        type="send"
+        message={{
+          content: 'Compartilhar os paranauê que eu aprender.',
+          type: 'string',
+        }}
+      />
+      <Message
+        type="receive"
+        message={{
+          content: 'Como posso te encontrar nas redes sociais?',
+          type: 'string',
+        }}
+      />
+      <Message
+        type="send"
+        message={{
+          content: <SocialMedias />,
+          type: 'node',
+        }}
+      />
+    </Layout>
+  )
+}
 
 export default memo(AboutPage)
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import SEO from '../components/Seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Home | Devzera" />
     <h1>Home</h1>
   </Layout>
 )
