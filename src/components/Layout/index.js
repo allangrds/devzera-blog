23a91b927d3e21de36582db5d68da74a68df9ac8
Allan Ramos <allangrds@gmail.com>
2020-06-04 10:50:22 -0300

    Create Posts “2020-06-04-expondo-seu-webserver-local-com-ngrok”

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,5 +1,6 @@
 import React, { Fragment, memo } from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 import './reset.css'
 import style from './style.module.css'
@@ -7,20 +8,34 @@ import style from './style.module.css'
 import Header from '../Header'
 import Menu from '../Menu'
 
-const Layout = ({ children }) => (
-  <Fragment>
-    <Header />
-    <main className={style.main}>
-      <div className={style.container}>
-        { children }
-      </div>
-    </main>
-    <Menu />
-  </Fragment>
-)
+const Layout = ({ children, whiteBackground }) => {
+  const mainClassName = classNames(
+    style.main,
+    {
+      [style.white]: whiteBackground,
+    },
+  )
+
+  return (
+    <Fragment>
+      <Header />
+      <main className={mainClassName}>
+        <div className={style.container}>
+          { children }
+        </div>
+      </main>
+      <Menu />
+    </Fragment>
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
+  whiteBackground: PropTypes.bool,
+}
+
+Layout.defaultProps = {
+  whiteBackground: false,
 }
 
 export default memo(Layout)
